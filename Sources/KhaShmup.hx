6fb2eb98cc210ccea65418e26a78f1de5967f71d
Justin Hamilton <justinanthonyhamilton@gmail.com>
2015-11-12 00:11:36 -0800

    First rendering

--- Sources/KhaShmup.hx
+++ Sources/KhaShmup.hx
@@ -1,9 +1,49 @@
 package ;
 
+import kha.Color;
+import kha.Configuration;
+import kha.Framebuffer;
 import kha.Game;
+import kha.Image;
+import kha.Loader;
+import kha.LoadingScreen;
+import kha.Scaler;
+import kha.Sys;
+import Ship;
 
 class KhaShmup extends Game {
+
+  private var backbuffer: Image;
+  private var ship: Ship;
+
   public function new() {
     super("KhaShmup", false);
   }
+
+  override public function init(): Void {
+    Configuration.setScreen(new LoadingScreen());
+    Loader.the.loadRoom("gameRoom", loadingFinished);
+  }
+
+  private function loadingFinished(): Void {
+    backbuffer = Image.createRenderTarget(width, height);
+    ship = new Ship(Std.int(width / 2) - Std.int(Ship.width / 2), 
+      Std.int(height / 2) - Std.int(Ship.height / 2), 
+      Loader.the.getImage("playerShip"));
+    Configuration.setScreen(this);
+  }
+
+  override public function render(framebuffer: Framebuffer): Void {
+    var g = backbuffer.g2;
+    
+    g.begin(Color.Black);
+    ship.render(g);
+    g.end();
+
+    startRender(framebuffer);
+    Scaler.scale(backbuffer, framebuffer, Sys.screenRotation);
+    endRender(framebuffer);
+  }
+
+
 }
---
+++ Sources/Ship.hx
@@ -0,0 +1,26 @@
+package ;
+
+import kha.graphics2.Graphics;
+import kha.Image;
+
+class Ship {
+  public static var width: Int = 99;
+  public static var height: Int = 75;
+
+  private var image: Image;
+
+  public var x: Int;
+  public var y: Int;
+
+  public function new(x: Int, y: Int, image: Image) {
+    this.x = x;
+    this.y = y;
+    this.image = image;
+  }
+
+  public function render(g: Graphics) {
+    if (image != null) {
+      g.drawImage(image, x, y);
+    }
+  }
+}
