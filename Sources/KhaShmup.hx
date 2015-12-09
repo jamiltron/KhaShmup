3744e42df2b91597b6616dc1b13808ce7bce691b
Justin Hamilton <justinanthonyhamilton@gmail.com>
2015-12-08 18:19:06 -0800

    Adding a few comments

--- Sources/KhaShmup.hx
+++ Sources/KhaShmup.hx
@@ -15,15 +15,18 @@ class KhaShmup {
   private var backbuffer: Image;
 
   public function new() {
+    // create a buffer to draw to
     backbuffer = Image.createRenderTarget(width, height);
   }
 
   public function render(framebuffer: Framebuffer): Void {
     var g = backbuffer.g2;
 
+    // clear our backbuffer using graphics2
     g.begin(bgColor);
     g.end();
 
+    // draw our backbuffer onto the active framebuffer
     framebuffer.g2.begin();
     Scaler.scale(backbuffer, framebuffer, System.screenRotation);
     framebuffer.g2.end();
